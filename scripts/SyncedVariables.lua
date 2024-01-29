cd8bd6a0527c839d5befe9fc25a275a3b37e2f06
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-01-28 20:52:33 -0700

    Update SyncedVariables.lua
    - Reorganized for readability

--- scripts/SyncedVariables.lua
+++ scripts/SyncedVariables.lua
@@ -1,16 +1,20 @@
--- Table setup
-local t = {}
-t.nV = false
-t.cF = false
+-- Setup table
+local t = {
+	nV = false,
+	cF = false
+}
 
 -- Night vision check
 local wasNV = t.nV
 function pings.nVPing(boolean)
+	
 	t.nV = boolean
+	
 end
 
 if host:isHost() then
 	function events.TICK()
+		
 		t.nV = false
 		for _, effect in ipairs(host:getStatusEffects()) do
 			if effect.name == "effect.minecraft.night_vision" then
@@ -21,22 +25,27 @@ if host:isHost() then
 			pings.nVPing(t.nV)
 		end
 		wasNV = t.nV
+		
 	end
 end
 
--- Creative flight setup
+-- Creative flight check
 local wasCF = t.cF
 function pings.cFPing(boolean)
+	
 	t.cF = boolean
+	
 end
 
 if host:isHost() then
 	function events.TICK()
+		
 		t.cF = host:isFlying()
 		if t.cF ~= wasCF then
 			pings.cFPing(t.cF)
 		end
 		wasCF = t.cF
+		
 	end
 end
 
