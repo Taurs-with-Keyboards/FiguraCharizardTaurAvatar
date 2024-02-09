798b139ea79b83594203d14734ed2f368cf3fe4d
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-02-08 23:08:32 -0700

    Update OriginPowers.lua
    - Removed all unused code
    - Added power check for being immune to fire

--- scripts/OriginPowers.lua
+++ scripts/OriginPowers.lua
@@ -1,27 +1,10 @@
 -- Required scripts
-require("lib.GSAnimBlend")
 local parts   = require("lib.GroupIndex")(models)
 local origins = require("lib.OriginsAPI")
 
--- Animations setup
-local anims = animations.CharizardTaur
-
-local t = {
-	createLight = false,
-	shootFire   = false,
-	flapWings   = false
-}
-
 function events.TICK()
 	
+	local power = origins.hasPower(player, "origins:charizard_fire_type")
+	renderer:setRenderFire(not power)
+	
 end
-
-function events.ON_PLAY_SOUND(id, pos, vol, pitch, loop, category, path)
-	if player:isLoaded() then
-		local atPos  = pos < player:getPos() + 5 and pos > player:getPos() - 5
-		local flapID = id == "minecraft:entity.ender_dragon.flap"
-		t.flapWings  = atPos and flapID and path and true or false
-	end
-end
-
-return t
