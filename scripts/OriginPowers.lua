172b62f66a856e7bfda23214914eb5f07ebe137e
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-03-05 10:28:00 -0700

    Update OriginPowers.lua
    - Fixed origin powers not working due to updated origin nameing

--- scripts/OriginPowers.lua
+++ scripts/OriginPowers.lua
@@ -3,7 +3,7 @@ local origins = require("lib.OriginsAPI")
 
 function events.TICK()
 	
-	local power = origins.hasPower(player, "origins:charizard_fire_type")
+	local power = origins.hasPower(player, "charizard:charizard_fire_type")
 	renderer:setRenderFire(not power)
 	
 end
