5c9220d831437a2cf19c21aa28555dd52103afc5
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-03-06 01:49:24 -0700

    Update OriginPowers.lua
    - Added comment

--- scripts/OriginPowers.lua
+++ scripts/OriginPowers.lua
@@ -3,6 +3,7 @@ local origins = require("lib.OriginsAPI")
 
 function events.TICK()
 	
+	-- Check if the fireproofing power is active
 	local power = origins.hasPower(player, "charizard:charizard_fire_type")
 	renderer:setRenderFire(not power)
 	
