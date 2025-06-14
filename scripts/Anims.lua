b1beae4ba169471cf26e84ce80038a0d39508096
Total <39064108+TotalTakeover@users.noreply.github.com>
2025-06-14 15:28:38 -0700

    Animation overhaul
    - Almost done!
    - I did remove the breathing animation, because I just felt like it wasn't needed
      - Users can add it back if they really want it

--- scripts/Anims.lua
+++ scripts/Anims.lua
@@ -1,22 +1,22 @@
 -- Required scripts
 require("lib.GSAnimBlend")
-local pokemonParts = require("lib.GroupIndex")(models.CharizardTaur)
-local ground       = require("lib.GroundCheck")
-local average      = require("lib.Average")
-local pose         = require("scripts.Posing")
+require("lib.Molang")
+local parts   = require("lib.PartsAPI")
+local ground  = require("lib.GroundCheck")
+local pose    = require("scripts.Posing")
+local effects = require("scripts.SyncedVariables")
 
 -- Animations setup
 local anims = animations.CharizardTaur
 
--- Variables setup
-local airTimer    = 0
-local shiverTimer = 0
+-- Variable
+local shiverStr = 0
 
 -- Parrot pivots
 local parrots = {
 	
-	pokemonParts.LeftParrotPivot,
-	pokemonParts.RightParrotPivot
+	parts.group.LeftParrotPivot,
+	parts.group.RightParrotPivot
 	
 }
 
@@ -33,40 +33,45 @@ end
 
 function events.TICK()
 	
-	-- Player variables
+	-- Variables
 	local vel      = player:getVelocity()
+	local dir      = player:getLookDir()
 	local onGround = ground()
 	
-	-- Ground timer
-	airTimer = not (onGround or player:isInWater()) and airTimer + 1 or 0
+	-- Directional velocity
+	local fbVel = player:getVelocity():dot((dir.x_z):normalize())
+	local lrVel = player:getVelocity():cross(dir.x_z:normalize()).y
+	local udVel = player:getVelocity().y
 	
-	-- Animation variables
-	local walking = pose.climb and vel:length() ~= 0 or vel.xz:length() ~= 0
-	local inAir   = airTimer > 15
+	-- Speed control
+	local walkSpeed   = math.clamp((pose.climb and udVel or fbVel < -0.05 and math.min(fbVel, math.abs(lrVel)) or math.max(fbVel, math.abs(lrVel))) * 6.5, -2, 2)
+	local flightSpeed = math.min(vel:length(), 2)
+	
+	-- Animation speeds
+	anims.walk:speed(walkSpeed)
+	anims.airFlying:speed(flightSpeed)
 	
 	-- Animation states
-	local groundIdle = ((not inAir or pose.climb) or player:getVehicle()) and not pose.swim and not pose.sleep 
-	local groundWalk = walking and (not inAir or pose.climb) and not pose.elytra and not pose.sleep
-	local airIdle    = inAir and not player:getVehicle() and not pose.elytra and not pose.climb
-	local airFlying  = (pose.elytra or pose.swim) and not onGround
+	local groundIdle = pose.crawl or not (effects.cF or pose.elytra or pose.swim or pose.sleep)
+	local walk       = (pose.climb and vel:length() ~= 0 or vel.xz:length() ~= 0) and not (effects.cF or pose.elytra or pose.sleep or player:getVehicle())
+	local airIdle    = effects.cF
+	local airFlying  = (pose.elytra or pose.swim) and not pose.crawl
 	local sleep      = pose.sleep
-	local breathe    = true
-	local shiver     = average(pokemonParts.Fire:getScale():unpack()) == 0
+	local shiver     = parts.group.Fire and parts.group.Fire:getScale():lengthSquared() / 3 == 0
 	
-	if shiver and shiverTimer < 200 then
-		shiverTimer = shiverTimer + 1
-	elseif not shiver and shiverTimer > 0 then
-		shiverTimer = shiverTimer - 1
-	end
+	-- Increase shiver strength
+	shiverStr = math.clamp(shiverStr + (shiver and 1 or -1), 0, 200)
+	
+	-- Animation blend
+	anims.shiver:blend(shiverStr / 200)
 	
 	-- Animations
 	anims.groundIdle:playing(groundIdle)
-	anims.groundWalk:playing(groundWalk)
+	anims.walk:playing(walk)
 	anims.airIdle:playing(airIdle)
 	anims.airFlying:playing(airFlying)
 	anims.sleep:playing(sleep)
-	anims.breathe:playing(breathe)
-	anims.shiver:playing(shiverTimer > 0)
+	anims.shiver:playing(shiverStr ~= 0)
 	
 end
 
@@ -79,23 +84,6 @@ local dirRot = {
 }
 
 function events.RENDER(delta, context)
-	
-	-- Player variables
-	local vel = player:getVelocity()
-	local dir = player:getLookDir()
-	
-	-- Directional velocity
-	local fbVel = player:getVelocity():dot((dir.x_z):normalize())
-	local lrVel = player:getVelocity():cross(dir.x_z:normalize()).y
-	local udVel = player:getVelocity().y
-	
-	-- Animation speeds
-	anims.groundWalk:speed(pose.climb and udVel * 6.5 or math.clamp((fbVel < -0.05 and math.min(fbVel, math.abs(lrVel)) or math.max(fbVel, math.abs(lrVel))) * 6.5, -2, 2))
-	anims.airFlying:speed(math.clamp(vel:length(), 0, 2))
-	anims.breathe:speed(math.min(vel:length() * 15 + 1, 8))
-	
-	-- Animation blend
-	anims.shiver:blend(math.map(shiverTimer, 0, 200, 0, 1))
 	
 	-- Sleep rotations
 	if pose.sleep then
@@ -122,21 +110,21 @@ function events.RENDER(delta, context)
 	
 	-- Parrot rot offset
 	for _, parrot in pairs(parrots) do
-		parrot:rot(-calculateParentRot(parrot:getParent()))
+		parrot:rot(-calculateParentRot(parrot:getParent()) - vanilla_model.BODY:getOriginRot())
 	end
 	
 	-- Crouch offset
 	local bodyRot = vanilla_model.BODY:getOriginRot(delta)
 	local crouchPos = vec(0, -math.sin(math.rad(bodyRot.x)) * 2, -math.sin(math.rad(bodyRot.x)) * 12)
-	pokemonParts.UpperBody:offsetPivot(crouchPos):pos(crouchPos.xy_ * 2)
-	pokemonParts.LowerBody:pos(crouchPos)
+	parts.group.UpperBody:offsetPivot(crouchPos):pos(crouchPos.xy_ * 2)
+	parts.group.LowerBody:pos(crouchPos)
 	
 end
 
 -- GS Blending Setup
 local blendAnims = {
 	{ anim = anims.groundIdle, ticks = {7,7} },
-	{ anim = anims.groundWalk, ticks = {7,7} },
+	{ anim = anims.walk,       ticks = {7,7} },
 	{ anim = anims.airIdle,    ticks = {7,7} },
 	{ anim = anims.airFlying,  ticks = {7,7} }
 }
@@ -153,7 +141,7 @@ function events.RENDER(delta, context)
 	
 	local rot = vanilla_model.HEAD:getOriginRot()
 	rot.x = math.clamp(rot.x, -90, 30)
-	pokemonParts.Spyglass:rot(rot)
+	parts.group.Spyglass:offsetRot(rot)
 		:pos(pose.crouch and vec(0, -4, 0) or nil)
 	
 end
