89a5971806a8a36ab52b0b92452beba714b41e81
Total <39064108+TotalTakeover@users.noreply.github.com>
2025-12-09 19:40:34 -0700

    Arm code cleanup

--- scripts/Anims.lua
+++ scripts/Anims.lua
@@ -10,8 +10,23 @@ local effects = require("scripts.SyncedVariables")
 -- Animations setup
 local anims = animations.CharizardTaur
 
+-- Config setup
+config:name("CharizardTaur")
+local armsMove = config:load("ArmsMove") or false
+
 -- Variable
 local shiverStr = 0
+
+-- Arms setup
+local leftArmLerp  = lerp:new(armsMove and 1 or 0, 0.5)
+local rightArmLerp = lerp:new(armsMove and 1 or 0, 0.5)
+
+-- Gets the origin rotation of a part, clamped
+local function getOriginRot(part, delta)
+	
+	return (vanilla_model[part]:getOriginRot(delta) + 180) % 360 - 180
+	
+end
 
 -- Parrot pivots
 local parrots = {
@@ -89,6 +104,27 @@ function events.TICK()
 	anims.sleep:playing(sleep)
 	anims.shiver:playing(shiverStr ~= 0)
 	
+	-- Arm variables
+	local handedness = player:isLeftHanded()
+	local mainL = not handedness and "OFF_HAND" or "MAIN_HAND"
+	local mainR = handedness and "OFF_HAND" or "MAIN_HAND"
+	local swingL = player:getSwingArm() == mainL
+	local swingR = player:getSwingArm() == mainR
+	local using = player:isUsingItem()
+	local active = player:getActiveHand()
+	local itemL = player:getHeldItem(not handedness)
+	local itemR = player:getHeldItem(handedness)
+	local usingL = using and active == mainL and itemL:getUseAction()
+	local usingR = using and active == mainR and itemR:getUseAction()
+	local bow = (usingL or usingR or ""):find("BOW") or (itemL:getTag().Charged or itemR:getTag().Charged) == 1
+	
+	-- Arms movement override
+	local armShouldMove = pose.swim or pose.crawl
+	
+	-- Arms movement targets
+	leftArmLerp.target  = (armsMove or armShouldMove or swingL or usingL or bow) and 0 or -1
+	rightArmLerp.target = (armsMove or armShouldMove or swingR or usingR or bow) and 0 or -1
+	
 	-- Set targets
 	if pose.elytra or pose.swim or pose.crawl or effects.cF then
 		lWing.target.yz = vec(0, 0)
@@ -155,23 +191,31 @@ function events.RENDER(delta, context)
 		
 	end
 	
+	-- Arm idle rotation
+	local idleTimer = world.getTime(delta)
+	local idleRot   = vec(math.deg(math.sin(idleTimer * 0.067) * 0.05), 0, math.deg(math.cos(idleTimer * 0.09) * 0.05 + 0.05))
+	
+	-- Apply arm rotations
+	parts.group.LeftArm:offsetRot((getOriginRot("LEFT_ARM", delta) + idleRot) * leftArmLerp.currPos)
+	parts.group.RightArm:offsetRot((getOriginRot("RIGHT_ARM", delta) - idleRot) * rightArmLerp.currPos)
+	
 	-- Apply wing bounce
 	parts.group.LeftWing1:offsetRot(lWing.currPos)
 	parts.group.RightWing1:offsetRot(rWing.currPos)
 	
 	-- Parrot rot offset
 	for _, parrot in pairs(parrots) do
-		parrot:rot(-calculateParentRot(parrot:getParent()) - vanilla_model.BODY:getOriginRot())
+		parrot:rot(-calculateParentRot(parrot:getParent()) - getOriginRot("BODY", delta))
 	end
 	
 	-- Crouch offset
-	local bodyRot = vanilla_model.BODY:getOriginRot(delta)
+	local bodyRot = getOriginRot("BODY", delta)
 	local crouchPos = vec(0, -math.sin(math.rad(bodyRot.x)) * 2, -math.sin(math.rad(bodyRot.x)) * 12)
 	parts.group.UpperBody:offsetPivot(crouchPos):pos(crouchPos.xy_ * 2)
 	parts.group.LowerBody:pos(crouchPos)
 	
 	-- Spyglass rotations
-	local headRot = vanilla_model.HEAD:getOriginRot()
+	local headRot = getOriginRot("HEAD", delta)
 	headRot.x = math.clamp(headRot.x, -90, 30)
 	parts.group.Spyglass:offsetRot(headRot)
 		:pos(pose.crouch and vec(0, -4, 0) or nil)
@@ -191,4 +235,86 @@ for _, blend in ipairs(blendAnims) do
 	if blend.anim ~= nil then
 		blend.anim:blendTime(table.unpack(blend.ticks)):blendCurve("easeOutQuad")
 	end
+end
+
+-- Arm movement toggle
+function pings.setAnimsArmsMove(boolean)
+	
+	armsMove = boolean
+	config:save("ArmsMove", armsMove)
+	
+end
+
+-- Sync variable
+function pings.syncAnims(a)
+	
+	armsMove = a
+	
+end
+
+-- Host only instructions
+if not host:isHost() then return end
+
+-- Sync on tick
+function events.TICK()
+	
+	if world.getTime() % 200 == 0 then
+		pings.syncAnims(armsMove)
+	end
+	
+end
+
+-- Required script
+local s, wheel, itemCheck, c = pcall(require, "scripts.ActionWheel")
+if not s then return end -- Kills script early if ActionWheel.lua isnt found
+
+-- Check for if page already exists
+local pageExists = action_wheel:getPage("Anims")
+
+-- Pages
+local parentPage = action_wheel:getPage("Main")
+local animsPage  = pageExists or action_wheel:newPage("Anims")
+
+-- Actions table setup
+local a = {}
+
+-- Actions
+if not pageExists then
+	a.pageAct = parentPage:newAction()
+		:item(itemCheck("jukebox"))
+		:onLeftClick(function() wheel:descend(animsPage) end)
+end
+
+a.armsAct = animsPage:newAction()
+	:item(itemCheck("red_dye"))
+	:toggleItem(itemCheck("rabbit_foot"))
+	:onToggle(pings.setAnimsArmsMove)
+	:toggled(armsMove)
+
+-- Update actions
+function events.RENDER(delta, context)
+	
+	if action_wheel:isEnabled() then
+		if a.pageAct then
+			a.pageAct
+				:title(toJson(
+					{text = "Animation Settings", bold = true, color = c.primary}
+				))
+		end
+		
+		a.armsAct
+			:title(toJson(
+				{
+					"",
+					{text = "Arm Movement Toggle\n\n", bold = true, color = c.primary},
+					{text = "Toggles the movement swing movement of the arms.\nActions are not effected.", color = c.secondary}
+				}
+			))
+		
+		for _, act in pairs(a) do
+			act:hoverColor(c.hover):toggleColor(c.active)
+		end
+		
+	end
+	
 end
